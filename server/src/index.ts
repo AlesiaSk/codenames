4e61fa7bb711af3f7867d67bd6c308643d32d45e
Alesya Skorokhod <askorohod@access.dev>
2023-01-07 23:34:42 +0300

    Code refactoring

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,13 +26,14 @@ let game = new Game(['Test1', 'Test2', 'Test3', 'Test4', 'Test5'], ['red', 'blue
 
 io.on('connection', (socket:any) => {
     socket.emit("words", game.words);
-    socket.emit("roles", game.wordsRoles);
+    socket.emit("roles", game.currentState);
     socket.on("checkCardTeam", (index: number) => {
         console.log(`Card ${index} is `, game.wordsRoles[index]);
         game.currentState[index] = game.wordsRoles[index];
-        io.emit("roles", game.wordsRoles);
+        io.emit("roles", game.currentState);
     });
     socket.on("disconnect", () => {
+        console.log('disconnected')
         game = new Game(['Test1', 'Test2', 'Test3', 'Test4', 'Test5'], ['red', 'blue', 'black', 'natural', 'red']);
     })
 });
